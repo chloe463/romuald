b47058f88e10543db376423c8a9429c6dec524ab
TsuyoshiHara <chloe463@gmail.com>
2018-04-22 02:14:41 +0900

    feat(armory): Add emit and remove delaydispatch
    Make it possible to pass function which create new state of the application
    and we don't need any reducer function.
    Keep `Armory.dispatch` for backward compatibility.

--- src/store.ts
+++ src/store.ts
@@ -1,50 +1,26 @@
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { Subscription } from 'rxjs/Subscription';
-import { Observable } from 'rxjs/Observable';
 
 import { Action } from './action';
 
-export class Armory<T> {
+export type Reducer<T> = (state: T, action: Action) => T;
 
-  private _currentState: T;
-  private _subject$: BehaviorSubject<T>;
+export class Armory<T> extends BehaviorSubject<T> {
 
-  constructor (
-    private _initialState: T,
-    private _reducer: any
-  ) {
-    this._currentState = _initialState;
-    this._subject$     = new BehaviorSubject<T>(_initialState);
-  }
+  public state: T;
 
-  public subscribe(fn: any): Subscription {
-    if (typeof fn !== 'function') {
-      throw 'Argument for subscribe must be function!';
-    }
-    return this._subject$.subscribe(fn);
+  constructor(public initialState: T, public reducer?: Reducer<T>) {
+    super(initialState);
+    this.state = initialState;
   }
 
   public dispatch(action: Action): void {
-    if (action.type === undefined || action.type === null) {
-      throw new Error('action must have \'type\'');
-    }
-    this._currentState = this._reducer(this._currentState, action);
-    this._subject$.next(this._currentState);
+    this.state = this.reducer(this.state, action);
+    this.next(this.state);
   }
 
-  public delayDispatch(fn: Promise<any>, action: Action): void {
-    fn.then((result) => {
-      this.dispatch({
-        type: action.type,
-        payload: Object.assign({}, action.payload, result)
-      });
-    }).catch((err) => {
-      console.error(err);
-      throw 'delayDispatch failed...';
-    });
+  public emit(fn: (state: T) => T) {
+    this.state = fn(this.state);
+    this.next(this.state);
   }
 
-  public next(action: Action): void {
-    this.dispatch(action);
-  }
 }
