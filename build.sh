be8dc62a24d8d04dea499022c818c21a85857d08
TsuyoshiHara <chloe463@gmail.com>
2018-05-23 01:55:29 +0900

    build: Add build script

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env sh
 
 # Remove previous build package
-rm -rf ./dist
+rm -rf ./builds
 
 # build
-npm run build
+yarn build:esm2015 && yarn build:fesm2015
+yarn build:esm5 && yarn build:fesm5
+yarn build:umd
 
 # Copy package.json to dist directory
-cp ./package.json ./dist/package.json
+cp ./package.json ./builds/package.json
 
 # Copy README
-cp ./README.md ./dist/README.md
+cp ./README.md ./builds/README.md
+
